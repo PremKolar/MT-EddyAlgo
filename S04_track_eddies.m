7f01f5fcf0088196122ba61f243f343790fb7680
niko <nkpmn@web.de>
2014-05-02 17:37:28 +0200

    void

--- S04_track_eddies.m
+++ S04_track_eddies.m
@@ -149,12 +149,10 @@ function [tracks,NEW]=append_tracked(TDB,tracks,OLD,NEW,sen)
 	if any(AIdx==0)
 		wrong=find(AIdx);
 		NN(wrong)=[];
-		idx(wrong)=[];
-		ID(wrong)=[];
 		IDc(wrong)=[];
 		AIdx(wrong)=[];
 		%	error('something wrong went wrong!!!');
-		warning('something wrong went wrong!!!');
+		warning('something wrong went wrong!!!'); %#ok<WNTAG>
 	end
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	

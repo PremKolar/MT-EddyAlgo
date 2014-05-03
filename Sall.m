df975cae306f65f1ca6a5780d447d9a8c86b5ac7
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-05-03 16:10:52 +0200

    void

--- Sall.m
+++ Sall.m
@@ -2,8 +2,8 @@
 %S01_contours
 %S01b_BruntVaisRossby
 %S02_infer_fields
-%S03_filter_eddies
-%S04_track_eddies
-%S05_init_output_maps
+S03_filter_eddies
+S04_track_eddies
+S05_init_output_maps
 S06_analyze_tracks
 S07_drawPlots

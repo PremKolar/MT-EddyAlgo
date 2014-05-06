96c6fe7a2980317b16f88788c171bfdc89acb9c7
niko <nkpmn@web.de>
2014-05-06 20:26:52 +0200

    void

--- S06_analyze_tracks.m
+++ S06_analyze_tracks.m
@@ -11,10 +11,10 @@ function S06_analyze_tracks
 	DD.threads.tracks=thread_distro(DD.threads.num,numel(DD.path.tracks.files));
 	%%
 	init_threads(DD.threads.num);
- 	spmd
+%  	spmd
 		id=labindex;
 		[map,tracks,vecs]=spmd_body(DD,id);
-	end
+% 	end
 	
 	%% merge
 	MAP=mergeMapData(map,DD);  %#ok<NASGU>

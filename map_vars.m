596d4123c9f283093f2bca3321daa97f325859c6
niko <nkpmn@web.de>
2014-05-27 20:14:17 +0200

    void

--- map_vars.m
+++ map_vars.m
@@ -1,9 +1,9 @@
 function MAP=map_vars
 	%% user input
-	MAP.geo.west=-60;
-   MAP.geo.east=-50;
-   MAP.geo.south=30;
-   MAP.geo.north=40;
+	MAP.geo.west=0;
+	MAP.geo.east=46;
+	MAP.geo.south=0;
+	MAP.geo.north=37;
 	MAP.time.delta_t = 1; % [days]
 	MAP.SSH_unitFactor = 100; % eg 100 if SSH data in cm, 1/10 if in deka m etc..
 	MAP.pattern.in='SSH_GLB_t.t0.1_42l_CORE.yyyymmdd.nc';

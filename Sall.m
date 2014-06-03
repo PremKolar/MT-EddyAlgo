fb0c2da5dd72a0e01df9f88322e8f55a99dcdb1f
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-06-03 03:51:58 +0200

    void

--- Sall.m
+++ Sall.m
@@ -1,11 +1,8 @@
-%S00_prep_data
-%S01_contours
-S01b_BruntVaisRossby
-% S01b_alternative
+S00_prep_data
+S01_contours
 S02_infer_fields
-%S03_filter_eddies
-%S04_track_eddies
-%S05_init_output_maps
-% S06a_getMeanU
- S06b_analyze_tracks
- S07_drawPlots
+S03_filter_eddies
+S04_track_eddies
+S05_init_output_maps
+S06_analyze_tracks
+S07_drawPlots
--- map_vars.m
+++ map_vars.m
@@ -1,12 +1,12 @@
 function MAP=map_vars
 	%% user input
-  MAP.geo.west=-80;
-  MAP.geo.east=-20;
-  MAP.geo.south=0;
-  MAP.geo.north=60;
-    MAP.time.delta_t = 1; % [days]
+	MAP.geo.west=0;
+	MAP.geo.east=46;
+	MAP.geo.south=0;
+	MAP.geo.north=37;
+	MAP.time.delta_t = 1; % [days]
 	MAP.SSH_unitFactor = 100; % eg 100 if SSH data in cm, 1/10 if in deka m etc..
-MAP.pattern.in='SSH_GLB_t.t0.1_42l_CORE.yyyymmdd.nc';
+	MAP.pattern.in='SSH_GLB_t.t0.1_42l_CORE.yyyymmdd.nc';
     MAP.pattern.lat='U_LAT_2D';
     MAP.pattern.lon='U_LON_2D';
     MAP.pattern.ssh='SSH';

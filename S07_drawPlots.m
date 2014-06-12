e78c5caf1dfc687a31114308d2f9892e546b78be
niko <nkpmn@web.de>
2014-06-12 16:09:03 +0200

    void

--- S07_drawPlots.m
+++ S07_drawPlots.m
@@ -8,8 +8,8 @@ function S07_drawPlots
     DD=initialise;
     %%	set ticks here!
     ticks.rez=300;
-    %      ticks.rez=42;
-    ticks.width=297/25.4*ticks.rez/5;
+          ticks.rez=42;
+    ticks.width=297/25.4*ticks.rez;
     ticks.height=ticks.width * DD.map.out.Y/DD.map.out.X;
     %         ticks.height=ticks.width/sqrt(2); % Din a4
     ticks.y= 0;

fb581e84e2d3d5fa2b8a8a9e5777a3c3468755e4
niko <nkpmn@web.de>
2014-06-17 19:06:56 +0200

    void

--- SUBS/savefig.m
+++ SUBS/savefig.m
@@ -5,52 +5,51 @@
 % Author:  NK
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function savefig(outdir,rez,xdim,ydim,tit,debug,frmt,saveFig)
-    if nargin<6,debug=false;end
-    if debug
-    disp('yo')
-    return
-	 end
-	 if nargin < 7
-		 frmt='dpng';
-	 end
-	 if nargin < 8
-		 saveFig=false;
-	 end
+	if nargin<6,debug=false;end
+	if debug,disp('yo');	return;	end
+	if nargin < 7,	frmt='dpng';	end
+	if nargin < 8,	saveFig=false;	end
 	fname=[outdir,tit];
-    mkdirp(outdir);	
+	mkdirp(outdir);
 	if rez==42
-		%% quick hack
-		disp(['quick print to ' [fname,'.png']])
-		print(gcf, '-dpng', [fname,'.png'])
-			
+		quickhack(fname)
 	else
 		%% set up figure
-		set(gcf,'renderer','opengl');
-		resolution=get(0,'ScreenPixelsPerInch');
-		xdim=xdim*rez/resolution;
-		ydim=ydim*rez/resolution;
-		set(gcf,'paperunits','inch','papersize',[xdim ydim]/rez,'paperposition',[0 0 [xdim ydim]/rez]);
-	%	xoy=xdim/ydim;
-		xa4=11.692*resolution;
-% 		ya4=xa4/xoy;			
-		fsScaled=round(14/xa4*xdim)		;
-% 			set(gcf,'paperunits','inch','position',[0 0 [xdim ydim]]);
-set(gca,'FontSize',fsScaled)
-set(findall(gcf,'type','text'),'FontSize',fsScaled)
-	
-		if strcmp(frmt,'dpdf')
-		eval(['print ',[fname,'.eps'] , ' -f -r',num2str(rez),' -depsc ;'])
-		system(['epstopdf ' fname '.eps']);
-		system(['rm ' fname '.eps']);
-		else
-			fnfull=[fname,'.',frmt(2:end)];
-			eval(['print ',fnfull , ' -f -r',num2str(rez),' -',frmt,';'])
-			system(['convert -density ' num2str(rez) 'x' num2str(rez) ' -resize ' num2str(xdim) 'x' num2str(ydim) ' quality 100 ' fnfull ' ' fname '.pdf' ]);
-		end
+		setupfigure(rez,xdim,ydim)
+		%% print
+		printStuff(frmt,fname,rez,xdim,ydim)
 	end
 	if saveFig
 		save(gcf,[fname,'.mat'])
 	end
-		close(gcf);
+	close(gcf);
 end
-
+function printStuff(frmt,fname,rez,xdim,ydim)
+	if strcmp(frmt,'dpdf')
+		eval(['print ',[fname,'.eps'] , ' -f -r',num2str(rez),' -depsc ;'])
+		system(['epstopdf ' fname '.eps']);
+		system(['rm ' fname '.eps']);
+	else
+		fnfull=[fname,'.',frmt(2:end)];
+		eval(['print ',fnfull , ' -f -r',num2str(rez),' -',frmt,';'])
+		system(['convert -density ' num2str(rez) 'x' num2str(rez) ' -resize ' num2str(xdim) 'x' num2str(ydim) ' quality 100 ' fnfull ' ' fname '.pdf' ]);
+	end
+end
+function setupfigure(rez,xdim,ydim)
+	set(gcf,'renderer','opengl');
+	resolution=get(0,'ScreenPixelsPerInch');
+	xdim=xdim*rez/resolution;
+	ydim=ydim*rez/resolution;
+	set(gcf,'paperunits','inch','papersize',[xdim ydim]/rez,'paperposition',[0 0 [xdim ydim]/rez]);
+	%	xoy=xdim/ydim;
+	xa4=11.692*resolution;
+	% 		ya4=xa4/xoy;
+	fsScaled=round(14/xa4*xdim)		;
+	% 			set(gcf,'paperunits','inch','position',[0 0 [xdim ydim]]);
+	set(gca,'FontSize',fsScaled)
+	set(findall(gcf,'type','text'),'FontSize',fsScaled)
+end
+function quickhack(fname)
+	disp(['quick print to ' [fname,'.png']])
+	print(gcf, '-dpng', [fname,'.png'])
+end
--- input_vars.m
+++ input_vars.m
@@ -1,27 +1,28 @@
 function U=input_vars
 	%% threads / debug
 	U.threads.num=12;
-	U.debugmode=false;
-	%     U.debugmode=true;
+	U.debugmode=0;
 	%% time
-	U.time.delta_t=1; % [days]!
-	U.time.from.str='19940102';
-	% 	 U.time.from.str='19940425';
-	%     U.time.till.str='19960730';
-	U.time.till.str='20040102';
+	%     U.time.from.str='19920114';  % min aviso
+	%     U.time.till.str='20130807'; % max aviso
+	% 	U.time.from.str='19940105';  % min pop
+	% 	U.time.till.str='20061231'; % max pop
+	U.time.from.str='19940105';  % min pop
+	U.time.till.str='19950105'; % max pop
+	U.time.delta_t=7; % [days]!
 	%% dirs
-	U.path.OutDirBaseName='EIchelt';
+	U.path.OutDirBaseName='aviwrld';	
 	U.path.TempSalt.name='../TempSalt/';
-	%     U.path.TempSalt.name='/media/ROM/TempSalt/';
-	U.path.raw.name='/scratch/uni/ifmto/u241194/DAILY/EULERIAN/SSH/';
-	%     U.path.raw.name='/media/ROM/SSH_POP/';
+	U.path.raw.name='/data/icdc/ocean/aviso_ssh/DATA/weekly/msla/';	
+% 	U.path.TempSalt.name='/home/niko/ROMnew/TempSalt/';
+% 	U.path.raw.name='/home/niko/ROMnew/SSH_POP/';
 	%% output MAP STUFF
-	U.map.out.X=20*2+1;
-	U.map.out.Y=20*2+1;
-	U.map.out.west=-120;
-	U.map.out.east=-100;
-	U.map.out.south=-60;
-	U.map.out.north=-40;
+	U.map.out.X=360*1+1;
+	U.map.out.Y=160*1+1;
+	U.map.out.west=-180;
+	U.map.out.east=180;
+	U.map.out.south=-80;
+	U.map.out.north=80;
 	%% input MAP STUFF
 	U.map.in.west=U.map.out.west;
 	U.map.in.east=U.map.out.east;
@@ -29,10 +30,11 @@ function U=input_vars
 	U.map.in.north=U.map.out.north;
 	U.map.in.time.delta_t = 1; % [days]
 	U.map.in.ssh_unitFactor = 100; % eg 100 if SSH data in cm, 1/10 if in deka m etc..
-	U.map.in.fname='SSH_GLB_t.t0.1_42l_CORE.yyyymmdd.nc';
-	U.map.in.keys.lat='U_LAT_2D';
-	U.map.in.keys.lon='U_LON_2D';
-	U.map.in.keys.ssh='SSH';
+	%% input patterns
+	U.map.in.fname='SsaltoDuacs__merged_msla__AVISO__ref__0.333deg__yyyymmdd.nc';
+	U.map.in.keys.lat='lat';
+	U.map.in.keys.lon='lon';
+	U.map.in.keys.ssh='msla';
 	U.TS.keys.lat='U_LAT_2D';
 	U.TS.keys.lon='U_LON_2D';
 	U.TS.keys.salt='SALT';
@@ -43,14 +45,14 @@ function U=input_vars
 	U.thresh.ssh_filter_size=1;
 	U.thresh.radius=0; % [SI]
 	U.thresh.amp=0.01; % [SI]
-	U.thresh.shape.iq=0.3; % isoperimetric quotient
-	U.thresh.shape.chelt=0.3; % (diameter of circle with equal area)/(maximum distance between nodes) (if ~switch.IQ)
-	U.thresh.corners=6; % min number of data points for the perimeter of an eddy
-	U.thresh.dist=.2*24*60^2; % max distance travelled per day
-	U.thresh.life=10; % min num of living days for saving
+	U.thresh.shape.iq=0.1; % isoperimetric quotient
+	U.thresh.shape.chelt=0.1; % (diameter of circle with equal area)/(maximum distance between nodes) (if ~switch.IQ)
+	U.thresh.corners=4; % min number of data points for the perimeter of an eddy
+	U.thresh.dist=1*24*60^2; % max distance travelled per day
+	U.thresh.life=3; % min num of living days for saving
 	U.thresh.ampArea=[.25 2.5]; % allowable factor between old and new time step for amplitude and area (1/4 and 5/1 ??? chelton)
 	%% switches
-	U.switchs.RossbyStuff=true;
+	U.switchs.RossbyStuff=true; % TODO
 	U.switchs.IQ=false;
 	U.switchs.chelt=true;
 	U.switchs.distlimit=true;
@@ -62,5 +64,5 @@ function U=input_vars
 	U.parameters.minProjecDist=150e3; % minimum linear_eccentricity*2 of ellipse (see chelton 2011)
 	U.parameters.trackingRef='CenterOfVolume'; % choices: 'centroid', 'CenterOfVolume', 'Peak'
 	%% technical params
-	U.RossbyStuff.splits =12; % number of chunks for brunt väis calculations	
+	U.RossbyStuff.splits =128; % number of chunks for brunt väis calculations
 end

2f9627812f74f3680ce58bd8e0eb18701cddc6f4
niko <nkpmn@web.de>
2014-06-19 13:47:35 +0200

    void

---
+++ SUBS/sec2day.m
@@ -0,0 +1,9 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Created: 18-Jun-2014 13:38:56
+% Computer:  GLNX86
+% Matlab:  7.9
+% Author:  NK
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function DD=sec2day(SS)
+	DD=SS/24/60/60;
+end

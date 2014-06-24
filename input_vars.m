72bdd93ed840fcffbbdd92500f27d9f966047a0a
niko <nkpmn@web.de>
2014-06-24 16:07:21 +0200

    void

--- input_vars.m
+++ input_vars.m
@@ -1,8 +1,8 @@
 function DD=input_vars
 	%% threads / debug
 	DD.threads.num=2;
-% 	DD.debugmode=false;
-	DD.debugmode=true;
+	DD.debugmode=false;
+% 	DD.debugmode=true;
 	%% time
 	DD.time.delta_t=3; % [days]!
 % 	DD.time.from.str='19091231';

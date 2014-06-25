f0c181709c2c6dd0b119019a9e30588af6d1954b
niko <nkpmn@web.de>
2014-06-25 10:03:38 +0200

    void

--- input_vars.m
+++ input_vars.m
@@ -12,7 +12,7 @@ function DD=input_vars
 	DD.time.till.str=datestr(datenum('19840814','yyyymmdd')+424242,'yyyymmdd');
 % 	DD.time.till.str='30000101';
 	%% dirs
-	DD.path.OutDirBaseName='new11';
+	DD.path.OutDirBaseName='new12';
 		DD.path.TempSalt.name='../TempSalt/';
 % 	    DD.path.TempSalt.name='~/ROMnew/TempSalt/';
 	DD.path.raw.name='../RawNewCdf/';

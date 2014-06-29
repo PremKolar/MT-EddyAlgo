d817ee34c90687c80b130d1e8fd22a923301608b
niko <nkpmn@web.de>
2014-06-29 04:47:34 +0200

    void

--- S07_getMeanU.m
+++ S07_getMeanU.m
@@ -11,58 +11,103 @@ function S07_getMeanU
 	[file]=findVelFiles(DD);
 	%% get dims
 	[d,pos,dim]=getDims(file,DD);
-%% means
+	%% means
 	means=getMeans(d,pos,dim,file,DD); %#ok<NASGU>
 	%% save
 	save([DD.path.meanU.file], 'means')
 	disp(['done!'])
-    conclude(DD);
+	conclude(DD);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function means=getMeans(d,pos,dim,file,DD)
-	for kk=1:numel(file)
-		disp(['found ' file(kk).U ' and ' file(kk).V])
-		U(:,:,kk)=squeeze(nc_varget(file(kk).U,DD.map.in.keys.U,dim.start,dim.length))/DD.parameters.meanUunit; %#ok<*AGROW>
-		V(:,:,kk)=squeeze(nc_varget(file(kk).V,DD.map.in.keys.V,dim.start,dim.length))/DD.parameters.meanUunit;
-		%%
-		x=DD.map.window.size.X;
-		y=DD.map.window.size.Y;
-		if x~=size(U,2) || y~=size(U,1)
-			warning('trivially resizing U/V data!!! ') %#ok<WNTAG>
-			sleep(5)
-			U=downsize(U,x,y);
-			V=downsize(V,x,y);
+		if DD.switchs.meanUviaOW
+		 [means]=GMzFromOWcase(file,DD);
+	else
+		[means]=GMzConstCase(file,DD);
 		end
-	end
-	disp(['creating means'])
-	U(U<-1e33)=nan; % missing values
-	V(V<-1e33)=nan; % missing values
-	means.zonal=nanmean(U,3);
-	means.merid=nanmean(V,3);
-	means.total=hypot(means.zonal,means.merid);
-	means.direc=azimuth(zeros(size(means.zonal)),zeros(size(means.zonal)),means.merid,means.zonal);
-	means.depth=d(pos.z.start);
-	%%
-	disp(['resizing to output size'])
-	lin=extractfield(load(DD.path.protoMaps.file),'idx');
-	means.small.zonal=nan(DD.map.out.Y,DD.map.out.X);
-	for li=unique(lin(lin~=0 & ~isnan(lin)))
-		means.small.zonal(li)=nanmean(means.zonal(lin==li));
+		
+		
+		
+		%-----------------------------------------------------------------------
+	function [means]=GMzFromOWcase(file,DD)
+		
+		
+		end
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	%-----------------------------------------------------------------------
+	function [means]=GMzConstCase(file,DD)
+		for kk=1:numel(file)
+			disp(['found ' file(kk).U ' and ' file(kk).V])
+			U(:,:,kk)=squeeze(nc_varget(file(kk).U,DD.map.in.keys.U,dim.start,dim.length))/DD.parameters.meanUunit; %#ok<*AGROW>
+			V(:,:,kk)=squeeze(nc_varget(file(kk).V,DD.map.in.keys.V,dim.start,dim.length))/DD.parameters.meanUunit;
+			%%
+			x=DD.map.window.size.X;
+			y=DD.map.window.size.Y;
+			if x~=size(U,2) || y~=size(U,1)
+				warning('trivially resizing U/V data!!! ') %#ok<WNTAG>
+				sleep(5)
+				U=downsize(U,x,y);
+				V=downsize(V,x,y);
+			end
+		end
+		disp(['creating means'])
+		U(U<-1e33)=nan; % missing values
+		V(V<-1e33)=nan; % missing values
+		means.zonal=nanmean(U,3);
+		means.merid=nanmean(V,3);
+		means.total=hypot(means.zonal,means.merid);
+		means.direc=azimuth(zeros(size(means.zonal)),zeros(size(means.zonal)),means.merid,means.zonal);
+		means.depth=d(pos.z.start);
+		%%
+		disp(['resizing to output size'])
+		lin=extractfield(load(DD.path.protoMaps.file),'idx');
+		means.small.zonal=nan(DD.map.out.Y,DD.map.out.X);
+		for li=unique(lin(lin~=0 & ~isnan(lin)))
+			means.small.zonal(li)=nanmean(means.zonal(lin==li));
+		end
 	end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [d,pos,dim]=getDims(file,DD)
-	dWanted=DD.parameters.meanU;
-	d=nc_varget(file(1).U,DD.map.in.keys.z);
-	[~,pos.z.start]=min(abs(d-dWanted));
-	pos.z.start=pos.z.start - 1; % starts at 0
-	pos.z.length=1;
-	pos.x.start=DD.map.window.limits.west - 1;
-	pos.x.length=DD.map.window.size.X;
-	pos.y.start=DD.map.window.limits.south-1;
-	pos.y.length=DD.map.window.size.Y;
-	dim.start = [0 pos.z.start pos.y.start pos.x.start];
-	dim.length = 	[inf pos.z.length pos.y.length pos.x.length ];
+	if DD.switchs.meanUviaOW
+		d=nan;
+		[pos,dim]=DIMzFromOWcase(DD);
+	else
+		[d,pos,dim]=DIMzConstCase(file,DD);
+	end
+	%-----------------------------------------------------------------------
+	function [d,pos,dim]=DIMzConstCase(file,DD)
+		dWanted=DD.parameters.meanU;
+		d=nc_varget(file(1).U,DD.map.in.keys.z);
+		[~,pos.z.start]=min(abs(d-dWanted));
+		pos.z.start=pos.z.start - 1; % starts at 0
+		pos.z.length=1;
+		pos.x.start=DD.map.window.limits.west - 1;
+		pos.x.length=DD.map.window.size.X;
+		pos.y.start=DD.map.window.limits.south-1;
+		pos.y.length=DD.map.window.size.Y;
+		dim.start = [0 pos.z.start pos.y.start pos.x.start];
+		dim.length = 	[inf pos.z.length pos.y.length pos.x.length ];
+	end
+	%-----------------------------------------------------------------------
+	function [pos,dim]=DIMzFromOWcase(DD)
+		pos.z.start=0;
+		pos.z.length=inf;
+		pos.x.start=DD.map.window.limits.west - 1;
+		pos.x.length=DD.map.window.size.X;
+		pos.y.start=DD.map.window.limits.south-1;
+		pos.y.length=DD.map.window.size.Y;
+		dim.start = [0 pos.z.start pos.y.start pos.x.start];
+		dim.length = 	[inf pos.z.length pos.y.length pos.x.length ];
+	end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [file]=findVelFiles(DD)

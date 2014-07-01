a66e32f80e851d29307edc76c14951cf1e6c77f2
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-07-01 19:54:08 +0200

    void

---
+++ SUBS/dispM.m
@@ -0,0 +1,13 @@
+function dispM(string,MasterOnly)
+    if nargin<2,MasterOnly=false;end    
+    if labindex==1
+        disp(['Master Thread: ']);
+        disp([string]);
+    else
+        if ~MasterOnly
+            commFile=sprintf('./.comm%03d.mat',labindex);
+            comm=matfile(commFile,'writable',true);
+            comm.printstack(end+1,1)={string};
+        end
+    end
+end

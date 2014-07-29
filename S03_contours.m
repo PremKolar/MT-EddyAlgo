8a3258efbe4de48a894c239208e8e5883d3f6d15
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-07-29 22:44:12 +0200

    void

--- S03_contours.m
+++ S03_contours.m
@@ -45,8 +45,7 @@ function II=get_contours(dd,TT)
         dispM([CONT.filename ' exists'])
         %         return
     end
-    %% loop over levels
-    disp('getting contours........')
+    %% loop over levels   
     CONT.all=contourc(II.grids.ssh,II.levels)';
     %% save data
     save(CONT.filename,'-struct','CONT');

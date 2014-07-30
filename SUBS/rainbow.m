5e7d57775c9210d3edd4ed765c53b5db7bfdaa45
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-07-30 21:41:34 +0200

    void

--- SUBS/rainbow.m
+++ SUBS/rainbow.m
@@ -1,22 +1,7 @@
-%function [colors]=rainbow(R, G, B, l, L)
-% om=2*pi/L;
-% 
-% 
-% red=R*sin(l*om-2*pi*(0/3));
-% green=G*sin(l*om-2*pi*(1/3));
-% blue=B*sin(l*om-2*pi*(2/3));
-% 
-% colors=[red green blue]/2+.5;
 function [colors]=rainbow(R, G, B, l, L)
-
-
-
-om=2*pi/L;
-
-
-red=R*sin(l*om-2*pi*(0/3));
-green=G*sin(l*om-2*pi*(1/3));
-blue=B*sin(l*om-2*pi*(2/3));
-
-colors=[red green blue]/2+.5;
-
+    om=2*pi/L;
+    red=sin(l*om-2*pi*(0/3));
+    green=sin(l*om-2*pi*(1/3));
+    blue=sin(l*om-2*pi*(2/3));
+    colors=([red green blue]/2+.5).*[R G B];
+end

ac3f57feaad09112f7e5cf1012937556815e1eab
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-07-30 14:05:35 +0200

    void

--- INPUT.m
+++ INPUT.m
@@ -13,12 +13,12 @@ function DD=INPUT
 %       DD.debugmode=true;
     %% time
     DD.time.from.str='19940101';
-    DD.time.till.str='19940201';
+    DD.time.till.str='19990201';
     %% window on globe
-    DD.map.in.west=-110;
-    DD.map.in.east= -110;
-    DD.map.in.south= -60;
-    DD.map.in.north= -50;
+    DD.map.in.west=-90;
+    DD.map.in.east= 0;
+    DD.map.in.south= 0;
+    DD.map.in.north= 80;
     %% output map res
     DD.map.out.X=90*1+1; % TODO
     DD.map.out.Y=80*1+1;
@@ -28,19 +28,19 @@ function DD=INPUT
     DD.thresh.radius=0; % [SI]
     DD.thresh.amp=0.01; % [SI]
     DD.thresh.shape.iq=0.2; % isoperimetric quotient
-    DD.thresh.shape.chelt=0.4; % (diameter of circle with equal area)/(maximum distance between nodes) (if ~switch.IQ)
+    DD.thresh.shape.chelt=0.2; % (diameter of circle with equal area)/(maximum distance between nodes) (if ~switch.IQ)
     DD.thresh.corners=6; % min number of data points for the perimeter of an eddy
     DD.thresh.dist=.1*24*60^2; % max distance travelled per day
     DD.thresh.life=3; % min num of living days for saving
     DD.thresh.amArea=[.25 2.5]; % allowable factor between old and new time step for amplitude and area (1/4 and 5/1 ??? chelton)
     %% switches
     DD.switchs.RossbyStuff=false;
-    DD.switchs.IQ=true;
-    DD.switchs.chelt=false;
+    DD.switchs.IQ=false;
+    DD.switchs.chelt=true;
     DD.switchs.distlimit=false;
     DD.switchs.AmpAreaCheck=false;
     DD.switchs.netUstuff=false;
-    DD.switchs.meanUviaOW=true;
+    DD.switchs.meanUviaOW=false;
     DD.switchs.spaciallyFilterSSH=false;
     DD.switchs.filterSSHinTime=true;
 end

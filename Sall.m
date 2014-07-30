ca4eb7842007b13c80f373a8ae05b4527829143f
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-07-30 13:38:16 +0200

    void

--- Sall.m
+++ Sall.m
@@ -4,9 +4,9 @@ S02_infer_fields
 S03_contours
 S04_filter_eddies
 S05_track_eddies
-maxOW
+% maxOW
 S06_init_output_maps
-S07_getMeanU
+% S07_getMeanU
 S08_analyze_tracks
 S09_drawPlots
-S10_makeAnimations
+% S10_makeAnimations

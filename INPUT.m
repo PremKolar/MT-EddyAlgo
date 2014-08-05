5eef422811202019b67da10ed3c06d86ec9af29a
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-08-05 16:41:13 +0200

    void

--- INPUT.m
+++ INPUT.m
@@ -15,20 +15,20 @@ function DD=INPUT
     DD.time.from.str='19900101';
      DD.time.till.str='19930101';
     %% window on globe
-    DD.map.in.west=-75;
-    DD.map.in.east= -50;
-    DD.map.in.south= 30;
-    DD.map.in.north= 45;
+    DD.map.in.west=-80;
+    DD.map.in.east= 0;
+    DD.map.in.south= 0;
+    DD.map.in.north= 70;
     %% output map res
-    DD.map.out.X=15*1+1; % TODO
-    DD.map.out.Y=15*1+1;
+    DD.map.out.X=80*1+1; % TODO
+    DD.map.out.Y=70*1+1;
     %% thresholds
     DD.contour.step=0.01; % [SI]
 %     DD.thresh.ssh_filter_size=1;
     DD.thresh.radius=0; % [SI]
     DD.thresh.maxRadiusOverRossbyL=10; % [SI]
     DD.thresh.amp=0.01; % [SI]
-    DD.thresh.shape.iq=0.5; % isoperimetric quotient
+    DD.thresh.shape.iq=0.2; % isoperimetric quotient
     DD.thresh.shape.chelt=0.2; % (diameter of circle with equal area)/(maximum distance between nodes) (if ~switch.IQ)
     DD.thresh.corners=10; % min number of data points for the perimeter of an eddy
 %     DD.thresh.dist=.1*24*60^2; % max distance travelled per day

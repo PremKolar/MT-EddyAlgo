05b6ea01048cb30d1d8740109f543bd594880270
niko <nkpmn@web.de>
2014-08-07 22:42:42 +0200

    void

--- SUBS/maxOWmain.m
+++ SUBS/maxOWmain.m
@@ -149,7 +149,7 @@ function buildRho(s,raw,Dim,threads)
 	for tt = s.timesteps
 		spmd(threads)
 			T=disp_progress('show',T,numel(s.timesteps),numel(s.timesteps))  ;
-			[temp,salt]=TSget(FileIn,keys,locCo);
+			[temp,salt]=TSget(s.Fin(tt+1),s.keys,locCo);
 			RHO=makeRho(salt,temp,sw_pres(depth,lat));
 		end
 		nc_varput(s.Fout{tt+1},'density',RHO{1},[0 0 0], [Dim.ws(1),Dim.ws(2),Dim.ws(3)]);

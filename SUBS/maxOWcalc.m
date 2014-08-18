c8f97c8d634375cd125583cdccc980bfb06cdaa0
niko <nkpmn@web.de>
2014-08-18 22:34:46 +0200

    void

--- SUBS/maxOWcalc.m
+++ SUBS/maxOWcalc.m
@@ -92,22 +92,25 @@ function defo = getDefo(uvg);dF
 	%     defo.stretch = uvg.dUdx - uvg.dVdy;
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function uvg = UVgrads(fname,f);dF
+function uvg = UVgrads(fname,repinZ);dF
 	m=matfile(fname);
 	dd.y= @(in)  diff(in,1,2);
 	dd.x= @(in)  diff(in,1,3);
 	z=size(m.UV.u,1);
-	uvg.dUdy = inxOry(dd.y(m.UV.u),'y',m.dy,z,f);
-	uvg.dUdx = inxOry(dd.x(m.UV.u),'x',m.dx,z,f);
-	uvg.dVdy = inxOry(dd.y(m.UV.v),'y',m.dy,z,f);
-	uvg.dVdx = inxOry(dd.x(m.UV.v),'x',m.dx,z,f);
+	uvg = getuvg(m.UV.u,m.UV.v,m.dy,m.dx,dd,repinZ,z);
 end
-function out=inxOry(in,inxy,dxy,z,f);dF
-	denom=f(dxy,z);
+function uvg=getuvg(u,v,dy,dx,dd,repinZ,z);dF
+	uvg.dUdy = inxOry(dd.y(u),'y',dy,z,repinZ);
+	uvg.dUdx = inxOry(dd.x(u),'x',dx,z,repinZ);
+	uvg.dVdy = inxOry(dd.y(v),'y',dy,z,repinZ);
+	uvg.dVdx = inxOry(dd.x(v),'x',dx,z,repinZ);
+end
+function out=inxOry(in,inxy,dxy,z,repinZ);dF
+	denom=repinZ(dxy,z);
 	if     strcmp(inxy,'y')
-		out=in( :,[1:end, end], : )./ denom;
+		out=in( :,[1:end, end], : )./denom;
 	elseif strcmp(inxy,'x')
-		out= in(:, :,[1:end, end])./ denom;
+		out= in(:, :,[1:end, end])./denom;
 	end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

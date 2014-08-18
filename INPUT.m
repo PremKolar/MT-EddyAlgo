2a00cb3d43e5400550cd89bd78d94388cbdf64e5
niko <nkpmn@web.de>
2014-08-18 12:18:00 +0200

    void

--- INPUT.m
+++ INPUT.m
@@ -11,8 +11,8 @@ function DD=INPUT
     DD.debugmode=false;
 %            DD.debugmode=true;
     %% time
-    DD.time.from.str='19940101';
-    DD.time.till.str='19960101';
+    DD.time.from.str='19900101';
+    DD.time.till.str='20100101';
     %% window on globe
     DD.map.in.west=-75;
     DD.map.in.east= -50;
@@ -29,7 +29,7 @@ function DD=INPUT
     DD.thresh.shape.iq=0.5; % isoperimetric quotient
     DD.thresh.shape.chelt=0.2; % (diameter of circle with equal area)/(maximum distance between nodes) (if ~switch.IQ)
     DD.thresh.corners=8; % min number of data points for the perimeter of an eddy
-    DD.thresh.life=20; % min num of living days for saving
+    DD.thresh.life=3*5; % min num of living days for saving
     DD.thresh.ampArea=[.25 2.5]; % allowable factor between old and new time step for amplitude and area (1/4 and 5/1 ??? chelton)
     DD.thresh.VolumeOverArea=[.2 2.5];
     %% switches

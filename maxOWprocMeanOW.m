2ed7612dfd0d0372b928d9bc13aaee997a093528
niko <nkpmn@web.de>
2014-08-23 03:44:35 +0200

    void

--- maxOWprocMeanOW.m
+++ maxOWprocMeanOW.m
@@ -10,10 +10,14 @@ function logOwMean=main(NC)
 	codi=codistributor1d(3);
 	localNCget=@(in,codi) getLocalPart(codistributed(in,codi));
 	spmd
-		logOwSum=getLocalPart(nan(NC.S.Z,NC.S.Y,NC.S.X,codi));
+		
+		logOwSum=localNCget(nc_varget(NC.files(1).full,'OkuboWeiss'),codi);
+		nanflag=isnan(logOwSum);
+		logOwSum(nanflag)=0;
 		logOwCount=getLocalPart(ones(NC.S.Z,NC.S.Y,NC.S.X,codi));
+		
 		T=disp_progress('init','buildingmean')  ;
-		for tt=1:NC.S.T
+		for tt=2:NC.S.T
 			T=disp_progress('show',T,NC.S.T);
 			newOw=localNCget(nc_varget(NC.files(tt).full,'OkuboWeiss'),codi);
 			nanflag=isnan(newOw);
@@ -21,10 +25,9 @@ function logOwMean=main(NC)
 			logOwSum=logOwSum+newOw;
 			logOwCount(~nanflag)=logOwCount(~nanflag)+1;
 		end
-		logOwSum(logOwCount==0)=nan;
+		logOwSum(logOwCount==0)=-30;
 		logOwSum=gcat(logOwSum./logOwCount,3,1);
-		
 	end
-	logOwMean=logOwSum{1};
+	logOwMean=logOwSum{1};	
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

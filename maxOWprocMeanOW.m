1cf818fcf8f55f6394d51bdeee6bd4b5c6cacf7f
niko <nkpmn@web.de>
2014-08-23 02:29:36 +0200

    void

--- maxOWprocMeanOW.m
+++ maxOWprocMeanOW.m
@@ -15,24 +15,27 @@ end
 function logOwMean=main(NC)
 	codi=codistributor1d(3);
 	spmd
+		tic
 		logOwSum=getLocalPart(nan(NC.S.Z,NC.S.Y,NC.S.X,codi));
 		logOwCount=getLocalPart(ones(NC.S.Z,NC.S.Y,NC.S.X,codi));
-	end
-	for tt=1:NC.S.T
-		tic
-		fprintf('prog %02d%%\n',round(tt/NC.S.T*100))
-		newOw=spmdCoDisp(nc_varget(NC.files(tt).full,'OkuboWeiss'),codi);
 		toc
-		tic
-		spmd
+		lens=gcat(size(logOwCount,codi.Dimension));
+		ends=cumsum(lens);
+		strts=ends-lens + 1;
+		for tt=1:NC.S.T
+			tic
+			fprintf('prog %02d%%\n',round(tt/NC.S.T*100))
+			ncchunkS=[0 0 strts(labindex)];
+			ncchunkL=[0 0  lens(labindex)];
+			newOw=nc_varget(NC.files(tt).full,'OkuboWeiss',ncchunkS,ncchunkL);
+			toc
+			tic
 			nanflag=isnan(newOw);
 			newOw(nanflag)=0;
 			logOwSum=logOwSum+newOw;
 			logOwCount(~nanflag)=logOwCount(~nanflag)+1;
+			toc
 		end
-		toc
-	end
-	spmd
 		logOwSum(logOwCount==0)=nan;
 		logOwSum=gcat(logOwSum./logOwCount,3,1);
 	end

3495b9d3f83f04de06f072b42fba50146a29f6a9
niko <nkpmn@web.de>
2014-08-23 02:03:31 +0200

    void

--- maxOWprocMeanOW.m
+++ maxOWprocMeanOW.m
@@ -13,46 +13,36 @@ function maxOWprocMeanOW
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function logOwMean=main(NC)
+	codi=codistributor1d(3);
 	spmd
-		dispM('getting logOwSum')
-		tic
-		codi=codistributor1d(3);
-		logOwSum=nan(NC.S.Z,NC.S.Y,NC.S.X,codi);
-		toc
-		dispM('getting logOwCount')
-		tic
-		logOwCount=ones(NC.S.Z,NC.S.Y,NC.S.X,codi);
-		toc
+		logOwSum=getLocalPart(nan(NC.S.Z,NC.S.Y,NC.S.X,codi));
+		logOwCount=getLocalPart(ones(NC.S.Z,NC.S.Y,NC.S.X,codi));
 	end
 	for tt=1:NC.S.T
 		tic
 		fprintf('prog %02d%%\n',round(tt/NC.S.T*100))
-		newOw=spmdCoDisp(nc_varget(NC.files(tt).full,'OkuboWeiss'),3);
+		newOw=spmdCoDisp(nc_varget(NC.files(tt).full,'OkuboWeiss'),codi);
 		toc
 		tic
-		nanflag=isnan(newOw);
-		newOw(nanflag)=0;
-		logOwSum=logOwSum+newOw;
-		logOwCount(~nanflag)=logOwCount(~nanflag)+1;
+		spmd
+			nanflag=isnan(newOw);
+			newOw(nanflag)=0;
+			logOwSum=logOwSum+newOw;
+			logOwCount(~nanflag)=logOwCount(~nanflag)+1;
+		end
 		toc
 	end
-	logOwSum(logOwCount==0)=nan;
-	logOwSum=logOwSum./logOwCount;
-	logOwMean=gather(logOwSum);
+	spmd
+		logOwSum(logOwCount==0)=nan;
+		logOwSum=gcat(logOwSum./logOwCount,3,1);
+	end
+	logOwMean=logOwSum{1};
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % function [out,codisp,lim]=spmdCoDisp(in,dim)
-function [out]=spmdCoDisp(in,dim)
-	
+function [out]=spmdCoDisp(in,codi)
 	spmd
-		out=codistributed(in,codistributor1d(dim));
-		% 		codisp=getCodistributor(out);
-		% 		parti=getfield(codisp,'Partition');
-		% 		LIM.a=[1 cumsum(parti(1:end-1))];
-		% 		LIM.b=LIM.a + parti;
-		% 		lim.a=LIM.a(labindex);
-		% 		lim.b=LIM.b(labindex);
-		% 		lim.len=lim.b-lim.a+1;
+		out=getLocalPart(codistributed(in,codi));
 	end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

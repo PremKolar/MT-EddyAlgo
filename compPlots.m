e3feeabaebfc53f8b3d5a8b3b5315c807a2c5f8a
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-09-02 01:43:04 +0200

    void

---
+++ compPlots.m
@@ -0,0 +1,66 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Created: 15-Jul-2014 13:52:44
+% Computer:  GLNX86
+% Matlab:  7.9
+% Author:  NK
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function compPlots
+   
+%     dirs= {'iq5fortnight';'iq2';'iq4';'iq6';'iq8';'iq5';'iq5nonVoA';'ch400amparea'};
+    
+     dirs= {'iq5fortnight';'iq2';'iq4';'iq6';'iq8';'iq5';'iq5nonVoA';'ch400amparea'};
+   
+    
+    D=inIt(dirs);
+   
+    
+%     L=numel(dirs)
+%     spmd(L)
+%         ii=labindex
+%           longesttracks(D.basedir,D.out(ii),D.thresh.ampArea,ii);             
+%     end
+    
+     for ii=1:numel(D.out)          
+        longesttracks(D.basedir,D.out(ii),D.thresh.ampArea,ii);   
+     end
+   printoutsLT(D);
+   
+     
+     
+    
+    %%
+     LTscatter('longesttracks.mat')
+     
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function LTscatter(matname) 
+    load(matname)
+    
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function D=inIt(dirs)
+    addpath(genpath('./'))
+    flsh= @(x) deal(x{:});
+    dbstop if error
+    dbstop if warning
+    D=INPUT;
+    D.threads.num=init_threads(12);
+    D.here=pwd;
+    D.basedir=['/scratch/uni/ifmto/u300065/FINAL/aorStuff/'];
+    D.out(numel(dirs))=struct;
+    [D.out(:).name]=flsh(dirs);
+    [D.out(:).LTfile]=  flsh(cellfun(@(c) [D.basedir 'LT_' c '.mat'],dirs,'uniformoutput',false));
+%     [D.out(:).file]=  flsh(cellfun(@(c) [D.basedir 'tracks_' c '.mat'],dirs,'uniformoutput',false));
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function printoutsLT(D)
+    system(sprintf('pdfjam -o tmp.pdf crpd*pdf'))
+    outtit=[cat(2,D.out(:).name),'.pdf'];
+    system(['pdfcrop  --margins "1 3 1 1" tmp.pdf ' outtit])
+    delete('crpd*pdf')
+    delete('tmp.pdf')
+end

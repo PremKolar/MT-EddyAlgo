c071c8c62eee6a4697d60784288a6f65a2185e7b
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-09-01 16:55:03 +0200

    O

--- Sall.m
+++ Sall.m
@@ -2,16 +2,16 @@
 %S01_BruntVaisRossby
 %S02_infer_fields
 %S03_contours
-  S04_filter_eddies
-try
- S04b_analyzeEddyThresh
-end
+%   S04_filter_eddies
+% try
+%  S04b_analyzeEddyThresh
+% end
  S05_track_eddies
 %S06_init_output_maps
 % % % S07_getMeanU
-try
-S08_analyze_tracks
-end
+% try
+% S08_analyze_tracks
+% end
 % S09_drawPlots
 % S10_makeAnimations
 

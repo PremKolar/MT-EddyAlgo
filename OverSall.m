ee8b527a05c30ef5a36b8a14e787f3dc1686ecc8
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-09-02 15:24:54 +0200

    void

--- OverSall.m
+++ OverSall.m
@@ -3,9 +3,10 @@ function OverSall
     DR='/scratch/uni/ifmto/u300065/FINAL/smAor/';
     Fnows={'iq2';'iq6';'iq6dl';'iq6dlMr';'iq6dlMrIc';'iq8';'iq4';'ch'};
     Dnows=cellfun(@(c) ['data' c],Fnows,'uniformoutput',false);
-    
-    Rdata=[DR 'dataB/'];
-    
+   
+    dbstop if error
+    Rdata=[DR 'dataC/'];
+    %%
     for ii=1:numel(Fnows)
         dnow=Dnows{ii};
         mkdirp([DR  dnow])
@@ -13,61 +14,50 @@ function OverSall
         mkdirp([DR  fnow])
     end
     %%
-    
-    %%
     for ii=1:numel(Fnows)
         fnow=Fnows{ii};
         dnow=Dnows{ii};
+        catINPUT(fnow)
         if ii==1
-            todoPre(fnow) ;
+            todoPre;
         end
-        try todo(fnow,DR,dnow,Rdata);end       %#ok<*TRYNC>
-        try todoPost(fnow,DR,dnow,Rdata);end
+        todoCore(DR,dnow,Rdata)
+        try todoPost(DR,dnow,Rdata);end %#ok<*TRYNC>
     end
 end
-
-function todoPre(fnow)
-    todo=sprintf('cp INPUT%s.m INPUT.m',fnow);
-    disp(todo);
-    system(todo);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function catINPUT(fnow)
+    try system(['mv INPUT.m INPUTold.m']), end
+    system(['more INPUTupper.m > INPUT.m'])
+    system(['more INPUT' fnow '.m >> INPUT.m'])
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function todoPre
     S00b_prep_data
     S01_BruntVaisRossby
     S02_infer_fields
     S03_contours
 end
-
-function todo(fnow,DR,dnow,Rdata)
-    todo=sprintf('cp INPUT%s.m INPUT.m',fnow);
-    disp(todo);
-    system(todo);
-    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function todoCore(DR,dnow,Rdata)
     S04_filter_eddies
     S05_track_eddies
-    
+    system(['rm -rf ' DR dnow '/' 'EDDIES'])
+    system(['rm -rf ' DR dnow '/' 'TRACKS'])
     system(['mv ' Rdata 'EDDIES ' DR dnow '/'])
     system(['mv ' Rdata 'TRACKS ' DR dnow '/'])
     cutdir=[Rdata 'CUTS/'];
     cut=dir([cutdir 'CUT_*.mat']);
     mkdirp([DR dnow '/CUTS'])
     system(['cp ' cutdir cut(1).name ' ' DR dnow '/CUTS/'])
-    
 end
-
-
-
-function todoPost(fnow,DR,dnow,Rdata)
-    todo=sprintf('cp INPUT%s.m INPUT.m',fnow);
-    disp(todo);
-    system(todo);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function todoPost(DR,dnow,Rdata)
     S04b_analyzeEddyThresh
     S06_init_output_maps
     S08_analyze_tracks
     S09_drawPlots
-    
-    %     S10_makeAnimations
-    
     system(['mv ' Rdata 'ANALYZED ' DR dnow '/'])
-    
-    
 end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--- compPlots.m
+++ compPlots.m
@@ -5,41 +5,69 @@
 % Author:  NK
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function compPlots
-   
-%     dirs= {'iq5fortnight';'iq2';'iq4';'iq6';'iq8';'iq5';'iq5nonVoA';'ch400amparea'};
-    
-     dirs= {'iq5fortnight';'iq2';'iq4';'iq6';'iq8';'iq5';'iq5nonVoA';'ch400amparea'};
-   
-    
+    dirs={'iq2';'iq6';'iq6dl';'iq6dlMr';'iq6dlMrIc';'iq8';'iq4';'ch'};
+    dirs={'iq2'};
     D=inIt(dirs);
-   
     
-%     L=numel(dirs)
-%     spmd(L)
-%         ii=labindex
-%           longesttracks(D.basedir,D.out(ii),D.thresh.ampArea,ii);             
-%     end
     
-     for ii=1:numel(D.out)          
-        longesttracks(D.basedir,D.out(ii),D.thresh.ampArea,ii);   
-     end
-   printoutsLT(D);
-   
-     
-     
+    %     L=numel(dirs)
+    %     spmd(L)
+    %         ii=labindex
+    %           longesttracks(D.basedir,D.out(ii),D.thresh.ampArea,ii);
+    %     end
+    
+    for ii=1:numel(D.out)
+        %          longesttracks(D.basedir,D.out(ii),D.thresh.ampArea,ii);
+        LTscatter(D.basedir,D.out(ii),D.thresh.ampArea,ii)
+    end
+    %    printoutsLT(D);
+    
+    
+    
     
     %%
-     LTscatter('longesttracks.mat')
-     
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function LTscatter(matname) 
-    load(matname)
+    
     
 end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function LTscatter(basedir,dout,threshampArea,ii)
+    
+    
+    
+    all=collectScatD(basedir,dout);
+    
+   
+   
+    scatter(cat(2,all.aol),cat(2,all.lat))
+    scatter(cat(2,all.iq),cat(2,all.lat))
+    scatter(cat(2,all.iq),cat(2,all.aol),10,cat(2,all.lat))
+   
+    
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
+function all=collectScatD(basedir,dout)
+    fs=dir([basedir 'data' dout.name '/TRACKS/*.mat']);
+   
+   ff=1
+    while ff<numel(fs)    
+        try
+        file=[basedir 'data' dout.name '/TRACKS/' fs(ff).name];
+        MF=matfile(file);
+        trck=MF.trck;
+        all(ff).lat=extractdeepfield(trck,'geo.lat');
+        all(ff).aol=extractdeepfield(trck,'area.RadiusOverRossbyL');
+        all(ff).iq=extractdeepfield(trck,'isoper');
+        catch
+            continue
+        end
+        ff=ff+1
+    end
+    
+end
+function scatplots(LTdata)
+    
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function D=inIt(dirs)
     addpath(genpath('./'))
@@ -49,11 +77,11 @@ function D=inIt(dirs)
     D=INPUT;
     D.threads.num=init_threads(12);
     D.here=pwd;
-    D.basedir=['/scratch/uni/ifmto/u300065/FINAL/aorStuff/'];
+    D.basedir=['/scratch/uni/ifmto/u300065/FINAL/smAor/'];
     D.out(numel(dirs))=struct;
     [D.out(:).name]=flsh(dirs);
     [D.out(:).LTfile]=  flsh(cellfun(@(c) [D.basedir 'LT_' c '.mat'],dirs,'uniformoutput',false));
-%     [D.out(:).file]=  flsh(cellfun(@(c) [D.basedir 'tracks_' c '.mat'],dirs,'uniformoutput',false));
+    %     [D.out(:).file]=  flsh(cellfun(@(c) [D.basedir 'tracks_' c '.mat'],dirs,'uniformoutput',false));
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

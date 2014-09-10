0aee11457f184aab483c4ad9a1b8edd07c718933
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-09-10 16:19:32 +0200

    void

--- INPUT.m
+++ INPUT.m
@@ -9,7 +9,6 @@ function DD=INPUT
     %     DD.overwrite=true;
     %% time
     DD.time.from.str='19940102';
-<<<<<<< HEAD
     DD.time.till.str='20500101';    
     threshlife=8*3;
  %% window on globe
@@ -17,16 +16,6 @@ function DD=INPUT
     DD.map.in.east= 180;
     DD.map.in.south= -80;
     DD.map.in.north= 80;
-=======
-    DD.time.till.str='19941002';
-    %      threshlife=20*7
-    threshlife=5*3;
-    %% window on globe
-    DD.map.in.west=-70;
-    DD.map.in.east= -55;
-    DD.map.in.south= 35;
-    DD.map.in.north= 45;
->>>>>>> master
     %% output map res
     DD.map.out.X=15*1+1; % TODO
     DD.map.out.Y=10*1+1;
@@ -43,26 +32,17 @@ function DD=INPUT
     DD.thresh.ampArea=[.25 2.5]; % allowable factor between old and new time step for amplitude and area (1/4 and 5/1 ??? chelton)
     DD.thresh.IdentityCheck=[2];
     %% switches
-<<<<<<< HEAD
+
     DD.switchs.IQ=0;
     DD.switchs.chelt=1;
-=======
-    DD.switchs.IQ=1;
-    DD.switchs.chelt=2;
->>>>>>> master
     DD.switchs.RossbyStuff=1;
     DD.switchs.distlimit=1;
     DD.switchs.AmpAreaCheck=0;
     DD.switchs.netUstuff=0;
     DD.switchs.meanUviaOW=0;
     DD.switchs.IdentityCheck=1;
-<<<<<<< HEAD
     DD.switchs.maxRadiusOverRossbyL=1;
     DD.switchs.spaciallyFilterSSH=false;
     DD.switchs.filterSSHinTime=true;
-=======
-    DD.switchs.maxRadiusOverRossbyL=0;
-    DD.switchs.spaciallyFilterSSH=0;
-    DD.switchs.filterSSHinTime=1;
->>>>>>> master
+
 end

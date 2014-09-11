e683efda5cc4f338701176b723a2801d6a7cbe10
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-09-11 20:13:43 +0200

    void

--- INPUT.m
+++ INPUT.m
@@ -3,9 +3,9 @@ function DD=INPUT
     %% threads / debug
     DD.threads.num=12;
     DD.debugmode=false;
-    DD.debugmode=true;
+%     DD.debugmode=true;
     DD.overwrite=false;
-    %     DD.overwrite=true;
+        DD.overwrite=true;
     %% time
     DD.time.from.str='19940102';
     DD.time.till.str='19941002';
@@ -27,20 +27,20 @@ function DD=INPUT
     DD.thresh.shape.iq=0.55; % isoperimetric quotient
     DD.thresh.shape.chelt=0.2; % (diameter of circle with equal area)/(maximum distance between nodes) (if ~switch.IQ)
     DD.thresh.corners.min=12; % min number of data points for the perimeter of an eddy
-    DD.thresh.corners.max=5*2*pi*1e6*1e-4; % at dx ~1e-4 -> skip eddies(radius> ~5000km) , just for performance
+    DD.thresh.corners.max=1*2*pi*1e6*1e-4; % at dx ~1e-4 -> skip eddies(radius> ~5000km) , just for performance
     DD.thresh.life=threshlife; % min num of living days for saving
     DD.thresh.ampArea=[.25 2.5]; % allowable factor between old and new time step for amplitude and area (1/4 and 5/1 ??? chelton)
     DD.thresh.IdentityCheck=[2];
     %% switches
     DD.switchs.IQ=1;
-    DD.switchs.chelt=2;
+    DD.switchs.chelt=0;
     DD.switchs.RossbyStuff=1;
     DD.switchs.distlimit=1;
     DD.switchs.AmpAreaCheck=0;
     DD.switchs.netUstuff=0;
     DD.switchs.meanUviaOW=0;
     DD.switchs.IdentityCheck=1;
-    DD.switchs.maxRadiusOverRossbyL=0;
+    DD.switchs.maxRadiusOverRossbyL=1;
     DD.switchs.spaciallyFilterSSH=0;
     DD.switchs.filterSSHinTime=1;
 end
--- Sall.m
+++ Sall.m
@@ -4,8 +4,11 @@
  S02_infer_fields
 % S03_contours
  S04_filter_eddies
-% % s05plotsAnima
-% S05_track_eddies
-% S06_init_output_maps
-% S08_analyze_tracks
-% %S09_drawPlots
+ s05plotsAnima
+ S05_track_eddies
+ S06_init_output_maps
+ S08_analyze_tracks
+
+ S09_plotsNew
+ 
+ %  S09_drawPlots

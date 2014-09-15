8cb845bacf8e05dfeecd378e08fc2d5f1ac4e340
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-09-16 00:16:20 +0200

    void

--- INPUT.m
+++ INPUT.m
@@ -1,15 +1,22 @@
+% templates :
+% 'udef' - user defined in INPUTuserDef.m
+% 'pop' - template for POP SSH data
+% 'aviso' - template for AVISO SSH data
+% 'mad' - template for Madeleine's data
 function DD=INPUT
-    DD.template='aviso';
+    DD.template='pop';
     %% threads / debug
-    DD.threads.num=6;
+    DD.threads.num=12;
     DD.debugmode=false;
 %     DD.debugmode=true;
     DD.overwrite=false;
-    %         DD.overwrite=true;
+%        DD.overwrite=true;
     %% time
-    DD.time.from.str='19900102';
-    DD.time.till.str='20500101';
-    threshlife=8*3;
+    DD.time.from.str='19940105';
+    DD.time.till.str='20061231';
+% %     DD.time.till.str='20061231';
+    %      threshlife=20*7
+    threshlife=7*3;
     %% window on globe
     DD.map.in.west=-180;
     DD.map.in.east= 180;
@@ -25,24 +32,21 @@ function DD=INPUT
     DD.thresh.amp=0.01; % [SI]
     DD.thresh.shape.iq=0.55; % isoperimetric quotient
     DD.thresh.shape.chelt=0.2; % (diameter of circle with equal area)/(maximum distance between nodes) (if ~switch.IQ)
-    DD.thresh.corners.min=12; % min number of data points for the perimeter of an eddy
-    DD.thresh.corners.max=5*2*pi*1e6*1e-4; % at dx ~1e-4 -> skip eddies(radius> ~1000km) , just for performance
+    DD.thresh.corners.min=10; % min number of data points for the perimeter of an eddy
+    DD.thresh.corners.max=5*2*pi*1e6*1e-4; % at dx ~1e-4 -> skip eddies(radius> ~5000km) , just for performance
     DD.thresh.life=threshlife; % min num of living days for saving
     DD.thresh.ampArea=[.25 2.5]; % allowable factor between old and new time step for amplitude and area (1/4 and 5/1 ??? chelton)
     DD.thresh.IdentityCheck=[2.5];
     %% switches
+    DD.switchs.IQ=1;
+    DD.switchs.chelt=0;
     DD.switchs.RossbyStuff=1;
     DD.switchs.distlimit=1;
+    DD.switchs.AmpAreaCheck=0;
     DD.switchs.netUstuff=0;
     DD.switchs.meanUviaOW=0;
-    DD.switchs.spaciallyFilterSSH=0;
-    DD.switchs.filterSSHinTime=1;
-    %%
-    DD.switchs.IQ=1;
     DD.switchs.IdentityCheck=1;
     DD.switchs.maxRadiusOverRossbyL=1;
-    %%
-    DD.switchs.chelt=0;
-    DD.switchs.AmpAreaCheck=0;
+    DD.switchs.spaciallyFilterSSH=0;
+    DD.switchs.filterSSHinTime=1;
 end
-

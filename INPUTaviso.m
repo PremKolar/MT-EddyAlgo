546b90d04de98463d25d88105cee15f28c692349
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-09-16 00:07:33 +0200

    auto commit after refresh

--- INPUTaviso.m
+++ INPUTaviso.m
@@ -3,7 +3,6 @@ function DD=INPUTaviso
     DD.time.delta_t=7; % [days]!
     %% dirs
     [~,DD.path.OutDirBaseName]=fileparts(pwd);
-    
     DD.path.TempSalt.name='/scratch/uni/ifmto/u300065/TempSaltUV/';
     DD.path.UV.name='/scratch/uni/ifmto/u300065/TempSaltUV/';
     DD.path.full3d.name='/scratch/uni/ifmto/u300065/MONTHLY/';

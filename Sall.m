b8f7f6c816acc214f272356964e9512fd713247d
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-09-15 23:45:06 +0200

    void

--- Sall.m
+++ Sall.m
@@ -1,11 +1,32 @@
-% S00a_singleCdf2perDT
+% % S00a_singleCdf2perDT
 % S00b_prep_data
- S01_BruntVaisRossby
- S02_infer_fields
-% S03_contours
- S04_filter_eddies
+% s00c_fillCorruptCuts
+%  %S01_BruntVaisRossby
+%   S02_infer_fields
+%  S03_contours
+%  S04_filter_eddies
+% % s05plotsAnima
+% % S05_track_eddies
+% % S06_init_output_maps
+% % S08_analyze_tracks
+% % S09_plotsNew
+% % %  S09_drawPlots
+% %  S00b_prep_data
+% %  S01_BruntVaisRossby
+% %  S02_infer_fields
+% %  S03_contours
+% S04_filter_eddies
 % % s05plotsAnima
 % S05_track_eddies
-% S06_init_output_maps
+%  S06_init_output_maps
 % S08_analyze_tracks
-% %S09_drawPlots
+
+try
+    S09_plotsNew
+end
+try
+    S09_drawPlots
+end
+try
+    s05plotsAnima
+end

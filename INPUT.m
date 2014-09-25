40c830bf8b4b8db80d2f3d864dde220b31e86592
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-09-25 17:18:01 +0200

    void

--- INPUT.m
+++ INPUT.m
@@ -4,18 +4,19 @@
 % 'aviso' - template for AVISO SSH data
 % 'mad' - template for Madeleine's data
 function DD=INPUT
-    DD.template='aviso';
+    DD.template='pop';
     %% threads / debug
     DD.threads.num=12;
     DD.debugmode=false;
-    %     DD.debugmode=true;
+%      DD.debugmode=true;
     DD.overwrite=false;
-    %     DD.overwrite=true;
+%        DD.overwrite=true;
     %% time
     DD.time.from.str='19940105';
-   DD.time.till.str='20061231';
+%   DD.time.till.str='19991231';
+     DD.time.till.str='20061231';
     %      threshlife=20*7
-    threshlife=7*4;
+    threshlife=7*8;
     %% window on globe
     DD.map.in.west=-180;
     DD.map.in.east= 180;
@@ -30,21 +31,22 @@ function DD=INPUT
     DD.thresh.maxRadiusOverRossbyL=4; %!
     DD.thresh.amp=0.01; % [SI]
     DD.thresh.shape.iq=0.55; % isoperimetric quotient
+    DD.thresh.shape.chelt=0.2; % (diameter of circle with equal area)/(maximum distance between nodes) (if ~switch.IQ)
     DD.thresh.corners.min=8; % min number of data points for the perimeter of an eddy
     DD.thresh.corners.max=1e42; % dangerous..
     DD.thresh.life=threshlife; % min num of living days for saving
     DD.thresh.ampArea=[.25 2.5]; % allowable factor between old and new time step for amplitude and area (1/4 and 5/1 ??? chelton)
-    DD.thresh.IdentityCheck=[2.5]; % 1: perfect fit, 2: 100% change ie factor 2 in either sigma or amp
+    DD.thresh.IdentityCheck=[2.5];
     %% switches
-    DD.switchs.IQ=0;
-    DD.switchs.chelt=1;
+    DD.switchs.IQ=1;
+    DD.switchs.chelt=0;
     DD.switchs.RossbyStuff=1;
     DD.switchs.distlimit=1;
-    DD.switchs.AmpAreaCheck=1;
+    DD.switchs.AmpAreaCheck=0;
     DD.switchs.netUstuff=0;
     DD.switchs.meanUviaOW=0;
-    DD.switchs.IdentityCheck=0;
-    DD.switchs.maxRadiusOverRossbyL=0;
+    DD.switchs.IdentityCheck=1;
+    DD.switchs.maxRadiusOverRossbyL=1;
     DD.switchs.spaciallyFilterSSH=0;
     DD.switchs.filterSSHinTime=1;
 end

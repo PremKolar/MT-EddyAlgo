24bd20e712b836df54e248f785616d1a7b8938bf
niko <nkpmn@web.de>
2014-09-30 19:16:31 +0200

    void

--- INPUT.m
+++ INPUT.m
@@ -2,13 +2,13 @@
 function DD=INPUT
     DD.template='mad';
     %% threads / debug
-    DD.threads.num=4;
+    DD.threads.num=1;
     DD.debugmode=false;
-% DD.debugmode=true;
+ DD.debugmode=true;
      DD.overwrite=false;
    DD.overwrite=true;      
     %% time
-        threshlife=8*3;
+    threshlife=3*3;
     DD.time.from.str='19091231';
     DD.time.till.str='19160724';
    %% window on globe
@@ -24,12 +24,11 @@ function DD=INPUT
     DD.thresh.radius=0; % [SI]
     DD.thresh.maxRadiusOverRossbyL=4; %!
     DD.thresh.amp=0.01; % [SI]
-    DD.thresh.shape.iq=0.1; % isoperimetric quotient
+    DD.thresh.shape.iq=0.55; % isoperimetric quotient
     DD.thresh.shape.chelt=0.2; % (diameter of circle with equal area)/(maximum distance between nodes) (if ~switch.IQ)
-    DD.thresh.corners.min=12; % min number of data points for the perimeter of an eddy
+    DD.thresh.corners.min=8; % min number of data points for the perimeter of an eddy
      %% DANGEROUS !
     DD.thresh.corners.max=1e42; % at dx ~1e-4 -> skip eddies(radius> ~1000km) , just for performance
-    
     DD.thresh.life=threshlife; % min num of living days for saving
     DD.thresh.ampArea=[.25 2.5]; % allowable factor between old and new time step for amplitude and area (1/4 and 5/1 ??? chelton)
     DD.thresh.IdentityCheck=[2];

3ee695863c5b46f220b53899b58a4d4f7a82513a
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-10-28 17:08:06 +0100

    void

--- INPUT.m
+++ INPUT.m
@@ -4,11 +4,11 @@
 % 'aviso' - template for AVISO SSH data
 % 'mad' - template for Madeleine's data
 function DD=INPUT
-      DD.template='aviso';
+      DD.template='pop2avi';
     %% threads / debug
-    DD.threads.num=4;
+    DD.threads.num=12;
     DD.debugmode=false;
-    DD.debugmode=true;
+%             DD.debugmode=true;
     DD.overwrite=false;
 %     DD.overwrite=true;
     %% time
@@ -16,17 +16,17 @@ function DD=INPUT
       DD.time.till.str='19590105';
     threshlife=7*4;
     %% window on globe (0:360° system)
-    DD.map.in.west= -70;
-    DD.map.in.east= -40;
+    DD.map.in.west= 0;
+    DD.map.in.east= 360;
     DD.map.in.south= 0;
-    DD.map.in.north= 40;
+    DD.map.in.north= 50;
     %% thresholds
     DD.contour.step=0.01; % [SI]
     DD.thresh.radius=0; % [SI]
-    DD.thresh.maxRadiusOverRossbyL=4; % [ ]
+    DD.thresh.maxRadiusOverRossbyL=4; %[ ]
     DD.thresh.amp=0.01; % [SI]
     DD.thresh.shape.iq=0.55; % isoperimetric quotient [ ]
-    DD.thresh.corners.min=20; % min number of data points for the perimeter of an eddy[ ]
+    DD.thresh.corners.min=16; % min number of data points for the perimeter of an eddy[ ]
     DD.thresh.corners.max=1e42; % dangerous.. [ ]
     DD.thresh.life=threshlife; % min num of living days for saving [days]
     DD.thresh.ampArea=[.25 2.5]; % allowable factor between old and new time step for amplitude and area (1/4 and 5/2 ??? chelton)

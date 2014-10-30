c8ba2765ae9f5a2f80bec4d674e4209f26253c5c
laptop <nkpmn@web.de>
2014-10-30 18:39:03 +0100

    void

--- STrhoP/makePressure.m
+++ STrhoP/makePressure.m
@@ -73,7 +73,7 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function S=dir2(d)
 	S=dir(d);
-	[base,~,~]=fileparts(S);
+	[base,~,~]=fileparts(d);
 	for ii=1:numel(S)
 		S(ii).fullname=[base S(ii).name];
 	end

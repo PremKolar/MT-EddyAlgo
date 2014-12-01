53ac2eb18c354b932253ad4532182e270f28ecec
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-12-01 14:38:16 +0100

    void

--- Sall.m
+++ Sall.m
@@ -1,22 +1,22 @@
-while true
-	try
+%while true
+%	try
 		
 		%S00b_prep_data
 		%S01_BruntVaisRossby
 		%S02_infer_fields
 		%S03_contours
 		S04_filter_eddies
-		%%
-		% S05_track_eddies
-		% S06_init_output_maps
-		% S08_analyze_tracks
-		% S09_plotsNew
+%		%%
+ S05_track_eddies
+ S06_init_output_maps
+ S08_analyze_tracks
+ S09_plotsNew
 		
-	catch me
-		disp(me.message)
-		disp(' waiting 5')
-		sleep(5)
-		continue
-	end
-	break
-end
+%	catch me
+%		disp(me.message)
+%		disp(' waiting 5')
+%		sleep(5)
+%		continue
+%	end
+%	break
+%end

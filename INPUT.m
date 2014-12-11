06c01e28aef3ada77ff72df0bc19ae5d8d6bf101
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-12-11 18:04:18 +0100

    void

--- INPUT.m
+++ INPUT.m
@@ -8,7 +8,7 @@ function DD=INPUT
     %% threads / debug
     DD.threads.num=12;
     DD.debugmode=false;
-DD.debugmode=true;
+% DD.debugmode=true;
     DD.overwrite=false;
     %     DD.overwrite=true;
     %% time
@@ -19,7 +19,7 @@ DD.debugmode=true;
 %     DD.time.till.str  = dateplus(DD.time.from.str,1*365,f);
 %     DD.time.till.str='19950105';
 %     threshlife=20*7
-    threshlife=3*8; 
+    threshlife=7*8; 
     %% window on globe (0:360° system)
     DD.map.in.west= 0;
     DD.map.in.east= 360;

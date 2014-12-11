ebc7ff3b8737e06cf534e53fe5b7bcbc8722e58b
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-12-11 18:07:09 +0100

    void

--- Sall.m
+++ Sall.m
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-%  S00b_prep_data
-%  S01_BruntVaisRossby
-%  S02_infer_fields
-%%
-% S03_contours
-%  S04_filter_eddies
-%  S05_track_eddies
-%  S06_init_output_maps
- S08_analyze_tracks
- S09_plotsNew
-=======
 %while true
 %	try
 		
@@ -22,7 +10,6 @@
 % 		S06_init_output_maps
 		S08_analyze_tracks
 		S09_plotsNew
->>>>>>> pop7II
 		
 %	catch me
 %		disp(me.message)

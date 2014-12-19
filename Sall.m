e91c0f47b27d6ece1ef7d5aaf6d2e8d6614fb7d6
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2014-12-19 20:45:33 +0100

    void

--- Sall.m
+++ Sall.m
@@ -1,21 +1,9 @@
-%while true
-%	try
-		
-		%S00b_prep_data
-		%S01_BruntVaisRossby
-		%S02_infer_fields
-		%S03_contours
-		S04_filter_eddies
-	 S05_track_eddies
- S06_init_output_maps
- S08_analyze_tracks
- S09_plotsNew
-		
-%	catch me
-%		disp(me.message)
-%		disp(' waiting 5')
-%		sleep(5)
-%		continue
-%	end
-%	break
-%end
+% S00b_prep_data
+% S01_BruntVaisRossby
+% S02_infer_fields
+% S03_contours
+S04_filter_eddies
+S05_track_eddies
+S06_init_output_maps
+S08_analyze_tracks
+S09_plotsNew

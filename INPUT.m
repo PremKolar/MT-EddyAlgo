122a577587a932210d562ec3737063294444a1a7
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-01-14 12:08:19 +0100

    void

--- INPUT.m
+++ INPUT.m
@@ -6,10 +6,10 @@
 % 'pop2avi' -
 function DD=INPUT
     %DD.template='pop2avi';
-%    DD.template='aviso';
-    DD.template='pop';
+    DD.template='aviso';
+    %DD.template='pop';
     %% threads / debug
-    DD.threads.num = 12;
+    DD.threads.num = 2;
     DD.debugmode   = false;
 %     DD.debugmode = true;
     DD.overwrite   = false;

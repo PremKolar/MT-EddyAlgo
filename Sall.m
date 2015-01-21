10a142bdc0d2416925a3a177a605ba9d888a61a9
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-01-21 11:54:20 +0100

    void

--- Sall.m
+++ Sall.m
@@ -1,5 +1,5 @@
-%S00b_prep_data
-%s00c_fillCorruptCuts
+% S00b_prep_data
+% s00c_fillCorruptCuts
 % S01_BruntVaisRossby
 % S02_infer_fields
 % S03_contours
@@ -7,9 +7,9 @@ S04_filter_eddies
 S05_track_eddies
 S06_init_output_maps
 S08_analyze_tracks
-try
-    S09_plotsNew
-end
-try
-    S09_drawPlots
-end
+% try
+%     S09_plotsNew
+% end
+% try
+%     S09_drawPlots
+% end

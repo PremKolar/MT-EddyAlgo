3115e06e4f93aa9d07a5b94dba413e97baa6ea7f
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-01-21 12:01:35 +0100

    auto commit before refresh

--- Sall.m
+++ Sall.m
@@ -3,13 +3,14 @@
 % S01_BruntVaisRossby
 % S02_infer_fields
 % S03_contours
+sleep(3*60*60)
 S04_filter_eddies
 S05_track_eddies
 S06_init_output_maps
 S08_analyze_tracks
-try
-    S09_plotsNew
-end
-try
-    S09_drawPlots
-end
+% try
+%     S09_plotsNew
+% end
+% try
+%     S09_drawPlots
+% end

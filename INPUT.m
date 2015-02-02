0f9f571c540acdadd6946a1619fba573c9974ebe
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-02-02 18:22:41 +0100

    void

--- INPUT.m
+++ INPUT.m
@@ -5,8 +5,8 @@
 % 'mad' - template for Madeleine's data
 % 'pop2avi' -
 function DD=INPUT
-    %DD.template='pop2avi';
-   DD.template='aviso';
+    DD.template='pop2avi';
+%    DD.template='aviso';
 %     DD.template='pop';
     %% threads / debug
     DD.threads.num = 12;
@@ -16,8 +16,8 @@ function DD=INPUT
 %     DD.overwrite = true;
     %% time
     DD.time.from.str  = '19940105'; %first pop/avi
-%     DD.time.till.str  = '19990105'; %first pop/avi
-    DD.time.till.str  = '20061227'; % last pop/avi
+    DD.time.till.str  = '19970105'; %first pop/avi
+%     DD.time.till.str  = '20061227'; % last pop/avi
     DD.time.delta_t   = 7; % [days]!
     threshlife        = 7*8; % TODO
     %% window on globe (0:360° system)

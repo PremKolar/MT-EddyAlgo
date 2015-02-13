f3ca6cf0b9a635eb6ea812341c9cbdef344db307
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-02-13 14:12:27 +0100

    void

--- INPUT.m
+++ INPUT.m
@@ -6,9 +6,8 @@
 % 'pop2avi' -
 function DD=INPUT
     %DD.template='pop2avi';
-%    DD.template='aviso';
-%    DD.template='pop';
-    DD.template='pop2avi';
+   DD.template='aviso';
+%     DD.template='pop';
     %% threads / debug
     DD.threads.num = 12;
     DD.debugmode   = false;
@@ -17,6 +16,7 @@ function DD=INPUT
 %     DD.overwrite = true;
     %% time
     DD.time.from.str  = '19940105'; %first pop/avi
+%     DD.time.till.str  = '19990105'; %first pop/avi
     DD.time.till.str  = '20061227'; % last pop/avi
     DD.time.delta_t   = 7; % [days]!
     threshlife        = 7*8; % TODO
@@ -41,7 +41,7 @@ function DD=INPUT
      %% switches
 
     %% 1 for I    -    0 for II
-    DD.switchs.chelt = 0;
+    DD.switchs.chelt = 1;
 
     DD.switchs.AmpAreaCheck  =  DD.switchs.chelt;
     DD.switchs.IQ            = ~DD.switchs.chelt;

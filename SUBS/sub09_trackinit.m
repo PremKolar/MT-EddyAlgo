d1f24998a1f9fc27d4fe81d7fd740782f7b6ab08
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-03-24 17:51:57 +0100

    void

--- SUBS/sub09_trackinit.m
+++ SUBS/sub09_trackinit.m
@@ -5,18 +5,18 @@
 % Author:  NK
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function sub09_trackinit(DD)
-
+    
     senses.t=fieldnames(DD.path.analyzedTracks)';
     senses.s=DD.FieldKeys.senses;
     %%
-    for ss=2:2
+    for ss=1:2
         [sense,root,eds,toLoad]=inits(DD,senses,ss);
         %%
         single=sPmDstoof(DD,eds,root,toLoad);
         %%
         cats=buildOutStruct(single);
         %%
-%         cats.vel=makeVel(cats);
+        cats.vel=makeVel(cats);
         %%
         saveCats(cats,sense);
     end
@@ -32,22 +32,22 @@ function [sense,root,eds,toLoad]=inits(DD,senses,ss)
     tl={'peakampto_mean';'radiusmean'; 'lat'; 'lon'; 'velPP'; 'age';'cheltareaLe';'cheltareaLeff';'cheltareaL';'iq'};
     toLoad(numel(tl)).name=struct;
     [toLoad(:).name] = deal(tl{:});
-
+    
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function sngl=sPmDstoof(DD,eds,root,toLoad)
     JJ=thread_distro(DD.threads.num,numel(eds));
-
+    
     spmd(DD.threads.num)
         FF=JJ(labindex,1):JJ(labindex,2);
-%         T=disp_progress('init','blubb');
-        % -----------------------------------------------
-%         for ff=1:numel(FF)
-%             T=disp_progress('calc',T,diff(JJ(labindex,:))+1,100);
-%             currFile = [root eds(FF(ff)).name];
-%             sngl(ff)=load(currFile,toLoad(:).name);
-%         end
-        % -----------------------------------------------
+        T=disp_progress('init','blubb');
+        %         -----------------------------------------------
+        for ff=1:numel(FF)
+            T=disp_progress('calc',T,diff(JJ(labindex,:))+1,100);
+            currFile = [root eds(FF(ff)).name];
+            sngl(ff)=load(currFile,toLoad(:).name);
+        end
+        %         -----------------------------------------------
         T=disp_progress('init','blubb');
         for ff=1:numel(FF)
             T=disp_progress('calc',T,diff(JJ(labindex,:))+1,100);
@@ -58,7 +58,7 @@ function sngl=sPmDstoof(DD,eds,root,toLoad)
         sngl=gcat(sngl,2,1);
     end
     sngl=sngl{1};
-
+    
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function cats=buildOutStruct(single)
@@ -85,26 +85,26 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function saveCats(cats,sense)
     area2L=@(ar) sqrt(ar/pi);
-%     tmp=cats.radiusmean;         %#ok<*NASGU>
-%     save(['TR-' sense.s '-rad.mat'],'tmp')
-%     tmp=area2L(cats.cheltareaLe);
-%     save(['TR-' sense.s '-radLe.mat'],'tmp')
-%     tmp=area2L(cats.cheltareaL);
-%     save(['TR-' sense.s '-radL.mat'],'tmp')
-%     tmp=area2L(cats.cheltareaLeff);
-%     save(['TR-' sense.s '-radLeff.mat'],'tmp')
-%     tmp=cats.age;
-%     save(['TR-' sense.s '-age.mat'],'tmp')
-%     tmp=cats.lat;
-%     save(['TR-' sense.s '-lat.mat'],'tmp')
-%     tmp=cats.lon;
-%     save(['TR-' sense.s '-lon.mat'],'tmp')
-%     tmp=cats.vel;
-%     save(['TR-' sense.s '-vel.mat'],'tmp')
-%     tmp=cats.peakampto_mean;
-%     save(['TR-' sense.s '-amp.mat'],'tmp')
-%     tmp=cats.isoper;
-%     save(['TR-' sense.s '-iq.mat'],'tmp')
+    %     tmp=cats.radiusmean;         %#ok<*NASGU>
+    %     save(['TR-' sense.s '-rad.mat'],'tmp')
+    %     tmp=area2L(cats.cheltareaLe);
+    %     save(['TR-' sense.s '-radLe.mat'],'tmp')
+    %     tmp=area2L(cats.cheltareaL);
+    %     save(['TR-' sense.s '-radL.mat'],'tmp')
+    %     tmp=area2L(cats.cheltareaLeff);
+    %     save(['TR-' sense.s '-radLeff.mat'],'tmp')
+    %     tmp=cats.age;
+    %     save(['TR-' sense.s '-age.mat'],'tmp')
+    %     tmp=cats.lat;
+    %     save(['TR-' sense.s '-lat.mat'],'tmp')
+    %     tmp=cats.lon;
+    %     save(['TR-' sense.s '-lon.mat'],'tmp')
+    %     tmp=cats.vel;
+    %     save(['TR-' sense.s '-vel.mat'],'tmp')
+    %     tmp=cats.peakampto_mean;
+    %     save(['TR-' sense.s '-amp.mat'],'tmp')
+    %     tmp=cats.isoper;
+    %     save(['TR-' sense.s '-iq.mat'],'tmp')
     tmp=cats.maxUV;
     save(['TR-' sense.s '-maxUV.mat'],'tmp')
     tmp=cats.radiusmean;         %#ok<*NASGU>

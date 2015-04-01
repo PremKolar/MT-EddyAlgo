ac124957fb641c47b33ba8d02fb757421ca8602e
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-04-01 15:45:07 +0200

    void

--- INPUT.m
+++ INPUT.m
@@ -6,35 +6,25 @@
 % 'pop2avi' -
 function DD=INPUT
     %DD.template='pop2avi';
-    %    DD.template='aviso';
+%    DD.template='aviso';
     DD.template='pop';
     %% threads / debug
     DD.threads.num = 12;
     DD.debugmode   = false;
-    %     DD.debugmode = true;
+%     DD.debugmode = true;
     DD.overwrite   = false;
-    %     DD.overwrite = true;
+%     DD.overwrite = true;
     %% time
-
-
     DD.time.from.str  = '19940105'; %first pop/avi
-    DD.time.till.str  = '19970403';
-
-%     DD.time.from.str  = '19970405'
-%     DD.time.till.str  = '20000704'
-
-    %     DD.time.till.str  = '20000701';
-
-
-    %     DD.time.till.str  = '20061227'; % last pop/avi
-
-    DD.time.delta_t   = 2; % [days]!
-    threshlife        = 30; % TODO
+%     DD.time.till.str  = '20020105'; %first pop/avi
+    DD.time.till.str  = '20061227'; % last pop/avi
+    DD.time.delta_t   = 7; % [days]!
+    threshlife        = 7*8; % TODO
     %% window on globe (0:360° system)
-    DD.map.in.west  =  40;
-    DD.map.in.east  =  90;
-    DD.map.in.south = -50;
-    DD.map.in.north = -30;
+    DD.map.in.west  =  0;
+    DD.map.in.east  =  360;
+    DD.map.in.south = -80;
+    DD.map.in.north =  80;
     %% thresholds
     DD.contour.step                = 0.01; % [SI]
     DD.thresh.radius               = 0; % [SI]
@@ -47,8 +37,8 @@ function DD=INPUT
     DD.thresh.life                 = threshlife; % min num of living days for saving [days]
     DD.thresh.ampArea              = [.25 2.5]; % allowable factor between old and new time step for amplitude and area (1/4 and 5/2 ??? chelton)
     DD.thresh.IdentityCheck        = 2; % 1: perfect fit, 2: 100% change ie factor 2 in either sigma or amp
-    DD.thresh.phase                = 1; % max(abs(rossby phase speed)) [SI]
-    %% switches
+    DD.thresh.phase                = 0.2; % max(abs(rossby phase speed)) [SI]
+     %% switches
 
     %% 1 for I    -    0 for II
     DD.switchs.chelt = 0;
@@ -56,6 +46,8 @@ function DD=INPUT
     DD.switchs.AmpAreaCheck  =  DD.switchs.chelt;
     DD.switchs.IQ            = ~DD.switchs.chelt;
     DD.switchs.IdentityCheck = ~DD.switchs.chelt;
+
+
 
     %% TODO
     DD.switchs.netUstuff = 0;
